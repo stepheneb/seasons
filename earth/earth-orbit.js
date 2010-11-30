82bd1eae305cb72ae400d6f64021cf1b326c312b
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-11-30 02:16:48 -0500

    utility functions for generating earth orbit data

--- earth/earth-orbit.js
+++ earth/earth-orbit.js
@@ -1,3 +1,27 @@
+
+var earth_orbit_segments = 128;
+
+var circle = function(scale) {
+    var points = [];
+    var pi2 = Math.PI * 2;
+    var increment = pi2 / earth_orbit_segments;
+    var angle = 0;
+    if (typeof(scale) === "number") {
+        for (var i = 0; i < earth_orbit_segments; i++, angle += increment) {
+            points.push(Math.sin(angle) * scale, 0, Math.cos(angle) * scale);
+        }
+    } else {
+        for (var i = 0; i < earth_orbit_segments; i++, angle += increment) {
+            points.push(Math.sin(angle), 0, Math.cos(angle));
+        }
+    }
+    return points;
+}
+
+var earth_orbit_positions = circle(earth_orbital_radius_km);
+var earth_orbit_indices = [];
+for (var i = 0; i < earth_orbit_segments; i++) { earth_orbit_indices.push(i) };
+
 var earthOrbit = SceneJS.createNode({
     type: "library",    
     nodes: [
