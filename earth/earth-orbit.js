0d684432d42f41bad4e380375cb225ce8ea9ec5f
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-02-04 13:27:44 -0500

    create orbit indicators with SceneJS.disk

--- earth/earth-orbit.js
+++ earth/earth-orbit.js
@@ -61,17 +61,17 @@ var earthCircleOrbit = SceneJS.createNode({
                 {
                     type: "scale",
                     x: 1,
-                    y: 0,
+                    y: 1,
                     z: 1,
                     nodes: [ 
                         { 
                     
                             type: "material",
                     
-                            baseColor:          { r: 0.1, g: 0.7, b: 1.0 },
-                            specularColor:      { r: 0.1, g: 0.7, b: 1.0 },
-                            specular:           1.0,
-                            shine:              1.0,
+                            baseColor:          { r: 1.0, g: 0.8, b: 0.1 },
+                            specularColor:      { r: 1.0, g: 0.8, b: 0.1 },
+                            specular:           0.5,
+                            shine:              2.0,
                             emit:               1.0,
                     
                             nodes: [
@@ -84,16 +84,25 @@ var earthCircleOrbit = SceneJS.createNode({
                                     nodes: [ 
 
                                         {  },
-                                
+                                        
                                         {
-
-                                            type: "geometry",
-                                            id: "earthCircleOrbitGeometry",
-                                            primitive: "line-loop",
-
-                                            positions: earth_circle_orbit_positions,
-                                            indices: earth_circle_orbit_indices
-                                            
+                            
+                                            type: "disk", 
+                                            id: "earth-in-space-circular-orbital-path" ,                                         
+                                            radius: earth_orbital_radius_km,
+                                            inner_radius : earth_orbital_radius_km - 0.2,
+                                            height: earth_diameter_km / 49,
+                                            rings: 360
+                                        },
+                                    
+                                        {
+                            
+                                            type: "disk",                                           
+                                            id: "sun-earth-circular-orbital-path" ,                                         
+                                            radius: earth_orbital_radius_km,
+                                            inner_radius : earth_orbital_radius_km - (earth_diameter_km * 50),
+                                            height: earth_diameter_km * 99,
+                                            rings: 360
                                         }
                                     ]
                                 }
@@ -107,6 +116,29 @@ var earthCircleOrbit = SceneJS.createNode({
 });
 
 //  Elliptical Orbit
+
+var deg2rad = Math.PI/180;
+var min2rad = Math.PI/(180*60);
+var sec2rad = Math.PI/(180*60*60);
+var rad2deg = 180/Math.PI;
+var au2km = 149597870.7;
+var earthMass = 5.9736e24;        // km
+var earthRadius = 6378.1;         // km
+var earthOrbitalPeriod = 365.256363004; // days
+var earthRotationPeriod = 0.99726968;   // days
+var t_day = 0.0001;
+
+var earthOrbitData = {
+  aphelion: 1.01671388,
+  perihelion: 0.98329134,
+  semiMajorAxis: 1.00000261,
+  radius: 1.00,
+  period: 1.00,
+  inclination: 7.25*deg2rad,
+  eccentricity : 0.01671123,
+  longitude : 348.73936*deg2rad,
+  argument : 114.20783*deg2rad
+}
 
 var earth_ellipse_orbit_segments = 1024;
 
@@ -168,40 +200,51 @@ var earthEllipseOrbit = SceneJS.createNode({
                 {
                     type: "scale",
                     x: 1,
-                    y: 0,
+                    y: 1,
                     z: 1,
                     nodes: [ 
                         { 
                     
                             type: "material",
-                    
-                            baseColor:          { r: 1.0, g: 0.7, b: 0.1 },
-                            specularColor:      { r: 1.0, g: 0.7, b: 0.1 },
+
+                            baseColor:          { r: 0.1, g: 0.8, b: 2.0 },
+                            specularColor:      { r: 0.1, g: 0.8, b: 2.0 },
                             specular:           1.0,
-                            shine:              1.0,
-                            emit:               1.0,
-                    
+                            shine:              2.0,
+                            emit:               2.0,
+
                             nodes: [
                             
                                 {
 
                                     type: "selector",
                                     id: "earthEllipseOrbitSelector",
-                                    selection: [1],
+                                    selection: [2],
                                     nodes: [ 
 
                                         {  },
-                                
+
                                         {
-
-                                            type: "geometry",
-                                            id: "earthEllipseOrbitGeometry",
-                                            primitive: "line-loop",
-
-                                            positions: earth_ellipse_orbit_positions,
-                                            indices: earth_ellipse_orbit_indices
-                                            
-                                        }
+                                                                    
+                                            type: "disk", 
+                                            id: "earth-in-space-elliptical-orbital-path" ,                                         
+                                            radius: earth_orbital_radius_km,
+                                            semiMajorAxis: earthOrbitData.semiMajorAxis,
+                                            inner_radius : earth_orbital_radius_km - 0.2,
+                                            height: earth_diameter_km / 50,
+                                            rings: 360
+                                        },
+                                                                            
+                                        {
+                                                                    
+                                            type: "disk",                                           
+                                            id: "sun-earth-elliptical-orbital-path" ,                                         
+                                            radius: earth_orbital_radius_km,
+                                            semiMajorAxis: earthOrbitData.semiMajorAxis,
+                                            inner_radius : earth_orbital_radius_km - (earth_diameter_km * 50),
+                                            height: earth_diameter_km * 50,
+                                            rings: 360
+                                        }                                        
                                     ]
                                 }
                             ]
