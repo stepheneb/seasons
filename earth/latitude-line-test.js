6f9d11f41117a88771ce4b76a8250c5bc632bd7e
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-03-03 16:59:31 -0500

    move stationary node to first in camera nodes list

--- earth/latitude-line-test.js
+++ earth/latitude-line-test.js
@@ -50,6 +50,67 @@ SceneJS.createNode({
 
                     nodes: [
 
+                        // Simulate the milky-way with a stationary background sphere
+                        {
+                            type: "stationary",    
+    
+                            nodes: [
+    
+                                // Size of sky sphere
+                                {
+                                    type: "scale",
+                                    x: milky_way_apparent_radius,
+                                    y: milky_way_apparent_radius,
+                                    z: milky_way_apparent_radius,
+                                    nodes: [
+    
+                                        // Starry texture
+                                        {
+                                            type: "texture",
+                                            layers: [
+                                                {
+                                                    uri: "images/milky_way_panorama_3000x1500.jpg",
+                                                    wrapS: "clampToEdge",
+                                                    wrapT: "clampToEdge",
+                                                    applyTo:"baseColor",
+                                                    blendMode:"multiply"
+                                                }
+                                            ],
+                                            nodes: [
+    
+                                                // Material for texture to apply to
+                                                {
+                                                    type: "material",
+                                                    baseColor:      { r: 1.0, g: 1.0, b: 1.0 },
+                                                    specularColor:  { r: 0.0, g: 0.0, b: 0.0 },
+                                                    specular:       0.0,
+                                                    shine:          0.0,
+                                                    emit:           1.0,
+    
+                                                    nodes: [
+    
+                                                        // Tilt the milky way a little bit
+                                                        {
+                                                            type: "rotate",
+                                                            z: 1,
+                                                            angle: 45.0,
+                                                            nodes: [
+    
+                                                                // Sphere geometry
+                                                                {
+                                                                    type: "sphere"
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+
                         {
                             type: "light",
                             mode:                   "point",
@@ -61,7 +122,7 @@ SceneJS.createNode({
                             quadraticAttenuation: 0.0,
                             linearAttenuation: 0.0
                         },
-        
+
                         {
                             type: "light",
                             mode:                   "dir",
@@ -78,67 +139,6 @@ SceneJS.createNode({
                             diffuse:                true,
                             specular:               true,
                             dir:                    { x: 1.0, y: 0.0, z: 0.75 }
-                        },
-                        
-                        // Simulate the milky-way with a stationary background sphere
-                        {
-                            type: "stationary",    
-        
-                            nodes: [
-        
-                                // Size of sky sphere
-                                {
-                                    type: "scale",
-                                    x: milky_way_apparent_radius,
-                                    y: milky_way_apparent_radius,
-                                    z: milky_way_apparent_radius,
-                                    nodes: [
-        
-                                        // Starry texture
-                                        {
-                                            type: "texture",
-                                            layers: [
-                                                {
-                                                    uri: "images/milky_way_panorama_3000x1500.jpg",
-                                                    wrapS: "clampToEdge",
-                                                    wrapT: "clampToEdge",
-                                                    applyTo:"baseColor",
-                                                    blendMode:"multiply"
-                                                }
-                                            ],
-                                            nodes: [
-        
-                                                // Material for texture to apply to
-                                                {
-                                                    type: "material",
-                                                    baseColor:      { r: 1.0, g: 1.0, b: 1.0 },
-                                                    specularColor:  { r: 0.0, g: 0.0, b: 0.0 },
-                                                    specular:       0.0,
-                                                    shine:          0.0,
-                                                    emit:           1.0,
-        
-                                                    nodes: [
-        
-                                                        // Tilt the milky way a little bit
-                                                        {
-                                                            type: "rotate",
-                                                            z: 1,
-                                                            angle: 45.0,
-                                                            nodes: [
-        
-                                                                // Sphere geometry
-                                                                {
-                                                                    type: "sphere"
-                                                                }
-                                                            ]
-                                                        }
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
                         },
 
                         {
