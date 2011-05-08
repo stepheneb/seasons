9010f8d61e53751f9d324b7e7cc08871d62cf8ba
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-05-07 21:34:38 -0400

    more cities, color system used for graph markers

--- earth/cities.js
+++ earth/cities.js
@@ -1,25 +1,45 @@
+// nice web color picker: http://www.allscoop.com/tools/Web-Colors/web-colors.php
+
+var city_colors = {
+  bright_red:       '#ff0000',
+  dark_red:         '#770022',
+  bright_blue:      '#0000ff',
+  dark_blue:        '#004488',
+  bright_green:     '#00ff00',
+  dark_green:       '#445500',
+  bright_purple:    '#ff11ff',
+  dark_purple:      '#770099',
+  bright_orange:    '#ff7711',
+  dark_orange:      '#dd4400',
+  bright_turquoise: '#00bbbb',
+  dark_turquoise:   '#007777',
+};
+
 var cities = [
+
     {
-        name: "Vancouver",
-        country: "Canada",
-        active: false,
+        name: "Fairbanks",
+        country: "United States",
+        active: true,
+        color: city_colors.bright_red,
         location: {
-            signed_longitude: 123.1,
-            signed_latitude: 49.25,
-            latitude: 49.25,
+            signed_longitude: 147.43,
+            signed_latitude: 64.51,
+            latitude: 64.51,
             lat_dir: "N",
-            longitude: 123.1,
+            longitude: 147.43,
             long_dir: "W",
-            elev: 34
+            elev: 133
         },
-        average_temperatures: [2.1, 4.0, 4.9, 9.1, 12.7, 17.4, 19.6, 18.1, 15.7, 10.0, 7.1, 2.1],
-        source: "http://climate.weatheroffice.gc.ca/climateData/monthlydata_e.html?Prov=XX&timeframe=3&StationID=889&Month=1&Day=1&Year=2009&cmdB1=Go"
+        average_temperatures: [-24, -19.5, -13, -2, 9, 15, 16, 12.5, 7, -4, -16, -23],
+        source: "http://www.climatetemp.info/usa/fairbanks-alaska.html"
     },
 
     {
         name: "Anchorage",
         country: "United States",
-        active: true,
+        active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: 149.9,
             signed_latitude: 61.21,
@@ -34,9 +54,46 @@ var cities = [
     },
 
     {
-        name: "San Francisco",
+        name: "Vancouver",
+        country: "Canada",
+        active: false,
+        color: city_colors.bright_red,
+        location: {
+            signed_longitude: 123.1,
+            signed_latitude: 49.25,
+            latitude: 49.25,
+            lat_dir: "N",
+            longitude: 123.1,
+            long_dir: "W",
+            elev: 34
+        },
+        average_temperatures: [2.1, 4.0, 4.9, 9.1, 12.7, 17.4, 19.6, 18.1, 15.7, 10.0, 7.1, 2.1],
+        source: "http://climate.weatheroffice.gc.ca/climateData/monthlydata_e.html?Prov=XX&timeframe=3&StationID=889&Month=1&Day=1&Year=2009&cmdB1=Go"
+    },
+
+    {
+        name: "Washington, D.C.",
         country: "United States",
         active: true,
+        color: city_colors.bright_blue,
+        location: {
+            signed_longitude: 77.02,
+            signed_latitude: 38.54,
+            latitude: 38.54,
+            lat_dir: "N",
+            longitude: 77.02,
+            long_dir: "W",
+            elev: 22
+        },
+        average_temperatures: [3, 3.5, 7, 14, 19, 24, 26, 25, 21, 15, 9, 3.5],
+        source: "http://www.climatetemp.info/usa/washington-district-of-columbia.html"
+    },
+
+    {
+        name: "San Francisco",
+        country: "United States",
+        active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: 122.42,
             signed_latitude: 37.77,
@@ -54,6 +111,7 @@ var cities = [
         name: "Miami",
         country: "United States",
         active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: 80.25,
             signed_latitude: 25.75,
@@ -68,9 +126,28 @@ var cities = [
     },
 
     {
+        name: "Quito",
+        country: "Ecuador",
+        active: true,
+        color: city_colors.bright_green,
+        location: {
+            signed_longitude: 78.35,
+            signed_latitude: -0.15,
+            latitude: 0.15,
+            lat_dir: "S",
+            longitude: 78.35,
+            long_dir: "W",
+            elev: 2811
+        },
+        average_temperatures: [15, 14.5, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15],
+        source: "http://www.climatetemp.info/ecuador/"
+    },
+
+    {
         name: "Galapagos Island",
         country: "Chile",
-        active: true,
+        active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: 91.1,
             signed_latitude: -0.75,
@@ -88,6 +165,7 @@ var cities = [
         name: "Rio de Janeiro",
         country: "Brazil",
         active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: 43.25,
             signed_latitude: -27.75,
@@ -102,26 +180,28 @@ var cities = [
     },
 
     {
-        name: "Rio Gallegos",
-        country: "Argentina",
-        active: false,
+        name: "Canberra",
+        country: "Australia",
+        active: true,
+        color: city_colors.bright_purple,
         location: {
-            signed_longitude: 69.25,
-            signed_latitude: -51.65,
-            latitude: 51.65,
+            signed_longitude: -149.08,
+            signed_latitude: -35.15,
+            latitude: 35.15,
             lat_dir: "S",
-            longitude: 69.25,
-            long_dir: "W",
-            elev: 12
+            longitude: 149.08,
+            long_dir: "E",
+            elev: 559
         },
-        average_temperatures: [12.5, 11.5, 11, 8, 4, 1, 1, 2.5, 6, 9, 11, 12],
-        source: "http://www.climatetemp.info/argentina/rio-gallegos.html"
+        average_temperatures: [20.5, 20, 17, 14, 9, 7, 6, 7, 10, 13, 15, 18.5],
+        source: "http://www.climatetemp.info/australia/canberra.html"
     },
-    
+
     {
         name: "Melbourne",
         country: "Australia",
-        active: true,
+        active: false,
+        color: city_colors.bright_red,
         location: {
             signed_longitude: -144.95,
             signed_latitude: -37.8,
@@ -136,9 +216,28 @@ var cities = [
     },
     
     {
+        name: "Rio Gallegos",
+        country: "Argentina", 
+        active: true,
+        color: city_colors.bright_orange,
+        location: {
+            signed_longitude: 69.13,
+            signed_latitude: -51.38,
+            latitude: 51.38,
+            lat_dir: "S",
+            longitude: 69.13,
+            long_dir: "W",
+            elev: 22
+        },
+        average_temperatures: [12.5, 11.5, 11, 8, 4, 1, 1, 2.5, 6, 9, 11, 12],
+        source: "http://www.climatetemp.info/argentina/rio-gallegos.html"
+    },
+    
+    {
         name: "McMurdo Station",
         country: "Antarctica",
         active: true,
+        color: city_colors.bright_turquoise,
         location: {
             signed_longitude: -166.73,
             signed_latitude: -77.88,
@@ -148,9 +247,8 @@ var cities = [
             long_dir: "E",
             elev: 24
         },
-        average_temperatures: [-2.9, -9.5, -18.2, -20.7, -21.7, -23, -25.7, -26.1, -24.6, -18.9, -9.7, -3.4, -16.9],
+        average_temperatures: [-2.9, -9.5, -18.2, -20.7, -21.7, -23, -25.7, -26.1, -24.6, -18.9, -9.7, -3.4],
         source: "http://www.coolantarctica.com/Antarctica%20fact%20file/antarctica%20environment/climate_graph/vostok_south_pole_mcmurdo.htm"
-    },
-    
+    }
 
 ];
