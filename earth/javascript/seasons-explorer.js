6b3771c1e31c4643bcbb4cce2ef6414e2ca7e2d8
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-08-28 23:33:42 -0400

    add +/- buttons for month selection

--- earth/javascript/seasons-explorer.js
+++ earth/javascript/seasons-explorer.js
@@ -49,6 +49,24 @@ var earth_orbital_radius =        AU / scale_factor;
 var km =                     1.0 / scale_factor;
 var meter =                   km / 1000;
 
+
+var monthData = {
+    "jan": { index:  0, num:   1, short_name: 'JAN', long_name: 'January' },
+    "feb": { index:  1, num:   2, short_name: 'FEB', long_name: 'February' },
+    "mar": { index:  2, num:   3, short_name: 'MAR', long_name: 'March' },
+    "apr": { index:  3, num:   4, short_name: 'APR', long_name: 'April' },
+    "may": { index:  4, num:   5, short_name: 'MAY', long_name: 'May' },
+    "jun": { index:  5, num:   6, short_name: 'JUN', long_name: 'June' },
+    "jul": { index:  6, num:   7, short_name: 'JUL', long_name: 'July' },
+    "aug": { index:  7, num:   8, short_name: 'AUG', long_name: 'August' },
+    "sep": { index:  8, num:   9, short_name: 'SEP', long_name: 'September' },
+    "oct": { index:  9, num:  10, short_name: 'OCT', long_name: 'October' },
+    "nov": { index: 10, num:  11, short_name: 'NOV', long_name: 'Novemeber' },
+    "dec": { index: 11, num:  12, short_name: 'DEC', long_name: 'December' }
+};
+
+var monthNames = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
 var earth_orbital_radius = au2km / scale_factor;
 
 var milky_way_apparent_radius = earth_orbital_radius * 10;
@@ -4224,6 +4242,8 @@ function selectedCityHandler() {
 selected_city.onchange = selectedCityHandler;
 
 var choose_month = document.getElementById("choose-month");
+var previous_month = document.getElementById("previous-month");
+var next_month = document.getElementById("next-month");
 
 function chooseMonthHandler(event) {
     var mon = choose_month.value;
@@ -4231,6 +4251,24 @@ function chooseMonthHandler(event) {
 };
 
 choose_month.onchange = chooseMonthHandler;
+
+previous_month.addEventListener('click', function () {
+  var index = monthData[earth.month].index,
+      mon;
+  index = modulo(++index, 12);
+  mon = monthNames[index];
+  choose_month.value = mon;
+  setEarthPositionByMon(mon);
+});
+
+next_month.addEventListener('click', function () {
+  var index = monthData[earth.month].index,
+      mon;
+  index = modulo(--index, 12);
+  mon = monthNames[index];
+  choose_month.value = mon;
+  setEarthPositionByMon(mon);
+});
 
 var choose_tilt = document.getElementById("choose-tilt");
 var earth_tilt_quaternion = SceneJS.withNode("earth-tilt-quaternion");
